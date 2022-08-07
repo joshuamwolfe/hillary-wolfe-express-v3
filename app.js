35d90f1165ddc97a1b19f8cd426155142ec1d318
Joshua Wolfe <joshuamwolfe@gmail.com>
2022-08-07 15:07:14 -0400

    Initial commit.

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const _ = require("lodash");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  res.render("home");
+});
+
+app.get("/about", function (req, res) {
+  res.render("about");
+});
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function () {
+  console.log(`Server started on port ${port} successfully`);
+});
